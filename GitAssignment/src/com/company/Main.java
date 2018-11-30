5950b4a76ee51a0013abc9fadb4b6ac4465603e9
Moataz <mezomoataz@hotmail.com>
2018-11-30 22:42:33 +0200

    Disinct Array

--- GitAssignment/src/com/company/Main.java
+++ GitAssignment/src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	public static void Distinct(int []arr) {
+		String temp="";
+		String collect="";
+        boolean flag=false;
+        int x;
+        for (int i = 0; i < arr.length; i++) {
+        	collect+=arr[i];
+        	collect+=" ";}
+      ArrayList<String> values= new ArrayList<String>(Arrays.asList(collect.split(" ")));
+      ArrayList<String> add=new ArrayList<>();
+     for(int i=0;i<values.size();i++){
+    	 
+    	  x=add.indexOf(values.get(i));
+    	 if(x==-1)
+    		 add.add(values.get(i));
+        	
+        		
+        }
+     for(int i=0;i<add.size();i++)
+        System.out.print(add.get(i)+" ");
+
+           
+    }
     }
 }
