023053b8cf22c7c62b2c336196580a8263448da5
beshomedhat <beshomedhat8@gmail.com>
2018-11-30 21:48:36 +0200

    20150083

--- GitAssignment/src/com/company/Main.java
+++ GitAssignment/src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static void getSmallest()
+    {
+                System.out.println("Get Smallest");
+        System.out.println("Please Enter size of array");
+        int sz;
+        Scanner in = new Scanner(System.in);
+        sz = in.nextInt();
+        Integer [] arr = new Integer[sz];
+        System.out.println("Please Enter your array");
+        for (int i = 0 ; i < sz ; ++i){
+            arr[i] = in.nextInt();
+}
+        int t=0;
+        for(int i=0; i<sz; i++)
+        {
+            for(int j=i+1; j<sz; j++)
+            {
+                if(arr[i]<arr[j])
+                {
+                    t=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=t;
+                }
+            }
+        }
+        System.out.print("the smallest number is : "+ t );
+    }
     public static void main(String[] args) {
 	// write your code here
+        
     }
+    
 }
