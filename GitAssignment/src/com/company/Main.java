51817f6572989315ab1f823bf18db6815400f4ea
Marwa <marwa246868@yahoo.com>
2018-11-30 23:40:51 +0200

    most_repeated

--- GitAssignment/src/com/company/Main.java
+++ GitAssignment/src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
-
 public class Main {
+public static void get_most_reapeated(int[]arr)
+	    {
+	        int most = 0;
+	        int temp=0;
+	        int count=0,tempcount;
+	        
+	       
+	        
+	        
+	        for(int j=0;j<=arr.length-1;j++)
+	        {
+	        temp=arr[j];
+	        tempcount=0;
+	            for(int k=1;k<=arr.length-1;k++)
+	                {
+	                if(temp==arr[k])
+	                    {
+	                        tempcount++;
+	                    }   
+	                        if(count<tempcount)
+	                            {
+	                                most=arr[k];
+	                                    count=tempcount;
+	                            }
+	                }
 
+	        }
+	        System.out.println("the most repeated value is : "+most);
+	    }
+    
     public static void main(String[] args) {
-	// write your code here
+	 int[] values={3,4,3,7,2,1,1,1,4};
+	get_most_reapeated(values);
     }
 }
