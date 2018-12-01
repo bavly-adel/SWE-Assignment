ef27ec05a2714b1ac2e1577dd04d7219a824229f
HossamHoussien <hossam.houssien.a@gmail.com>
2018-12-01 22:10:59 +0200

    Changed parameter type to int and fix change input array

--- GitAssignment/src/com/company/Main.java
+++ GitAssignment/src/com/company/Main.java
@@ -7,26 +7,27 @@ public class Main {
     }
 	
 
-    public static double getMedian(double[] input){
+    public static int getMedian(int[] input){
 
         // Median of array of N values
         // If N is odd  => The median will be at (N + 1) / 2 (middle value)
         // If N is even => The median will be the average of (N/2 , N/2 + 1)
-
-        int len = input.length;
+        
+        int [] temp = Arrays.copyOf(input, input.length);
+        int len = temp.length;
         boolean even = (len % 2) == 0;
         int medianPosition = even ? (int) (len / 2.0) :  ((len + 1) / 2) - 1;
 
         // SORT THE INPUT ARRAY BEFORE SELECTING THE MEDIAN
          for (int i = 0 ; i < len-1 ; i++)
              for (int j = i + 1; j < len  ; j++)
-                 if (input[i] > input[j]){
-                     double temp = input[j];
-                     input[j] = input[i];
-                     input[i] = temp;
+                 if (temp[i] > temp[j]){
+                     int x = temp[j];
+                     temp[j] = temp[i];
+                     temp[i] = x;
                  }
 
         // RETURN THE MEDIAN
-        return even ? (input[medianPosition] + input[medianPosition + 1] / 2.0) : input [medianPosition];
+        return even ? (temp[medianPosition] + temp[medianPosition + 1] / 2) : temp [medianPosition];
     }
 }
