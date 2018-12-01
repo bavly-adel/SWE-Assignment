1402076a5146c604bfb691a55bcc04e21dd170b9
basharreda <45491952+basharreda@users.noreply.github.com>
2018-12-01 16:20:35 +0200

    Update Main.java

--- GitAssignment/src/com/company/Main.java
+++ GitAssignment/src/com/company/Main.java
@@ -54,30 +54,22 @@ public static void get_most_reapeated(int[]arr)
         return min_3element;
     }
 
-    public static void getSmallest()
+        public static void getSmallest(int []arr)
     {
                 System.out.println("Get Smallest");
-        System.out.println("Please Enter size of array");
-        int sz;
-        Scanner in = new Scanner(System.in);
-        sz = in.nextInt();
-        Integer [] arr = new Integer[sz];
-        System.out.println("Please Enter your array");
-        for (int i = 0 ; i < sz ; ++i){
-            arr[i] = in.nextInt();
-}
-        int t=0;
+        
+        int sz = arr.length;
+
+        int t=arr[0];
         for(int i=0; i<sz; i++)
         {
-            for(int j=i+1; j<sz; j++)
-            {
-                if(arr[i]<arr[j])
+
+                if(arr[i]<t)
                 {
                     t=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=t;
+
                 }
-            }
+            
         }
         System.out.print("the smallest number is : "+ t );
     }
